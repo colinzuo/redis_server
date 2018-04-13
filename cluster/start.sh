45e9ee243fd931325ec0ec77bc3fe301eec87085
Colin Zuo <colinzuo@qq.com>
2018-04-13 09:15:36 +0800

    use redis-trib to create cluster

--- cluster/start.sh
+++ cluster/start.sh
@@ -1,7 +1,10 @@
 ./stop.sh
 
+./cleanup.sh
+
 SERVER_CMD="../../cluster/bin/redis-server"
 REDIS_CFG="./redis_cluster.conf"
+SERVER_IP="172.27.0.5"
 
 cd node1
 nohup ${SERVER_CMD} ${REDIS_CFG} &
@@ -14,3 +17,7 @@ cd -
 cd node3
 nohup ${SERVER_CMD} ${REDIS_CFG} &
 cd -
+
+./bin/redis-trib.rb create ${SERVER_IP}:7000 ${SERVER_IP}:7001 ${SERVER_IP}:7002 <<EOF
+yes
+EOF
